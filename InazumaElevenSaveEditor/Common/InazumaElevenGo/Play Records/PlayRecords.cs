2201bcc7dd4b201493f696b9cfb35526bd8dca33
Tinifan <30804632+Tiniifan@users.noreply.github.com>
2022-09-05 03:34:03 +0200

    Add Galaxy ressources

--- InazumaElevenSaveEditor/Common/InazumaElevenGo/Play Records/PlayRecords.cs
+++ InazumaElevenSaveEditor/Common/InazumaElevenGo/Play Records/PlayRecords.cs
@@ -6,7 +6,7 @@ namespace InazumaElevenSaveEditor.Common.InazumaElevenGo
 {
     public static class PlayRecords
     {
-        //  Missing GO and Galaxy Play Records
+        //  Missing GO Play Records
 
         public static Dictionary<int, List<PlayRecord>> Cs = new Dictionary<int, List<PlayRecord>>
         {
@@ -121,6 +121,199 @@ namespace InazumaElevenSaveEditor.Common.InazumaElevenGo
                     new PlayRecord("In Demand"),
                     new PlayRecord("Up to the Challenge"),
                     new PlayRecord("Treasure Master")
+                }
+            },
+        };
+
+        public static Dictionary<int, List<PlayRecord>> Galaxy = new Dictionary<int, List<PlayRecord>>
+        {
+            {0, new List<PlayRecord>()
+                {
+                    new PlayRecord("Amateur Snapper"),
+                    new PlayRecord("Photographer"),
+                    new PlayRecord("Pro Photographer"),
+                    new PlayRecord("Chatter"),
+                    new PlayRecord("Gossiper"),
+                    new PlayRecord("Conversationalist"),
+                    new PlayRecord("Emblem Collector")
+                }
+            },
+            {1, new List<PlayRecord>()
+                {
+                    new PlayRecord("Emblem Enthusiast"),
+                    new PlayRecord("Emblem Lover"),
+                    new PlayRecord("Kit Collector"),
+                    new PlayRecord("Kit Enthusiast"),
+                    new PlayRecord("Kit Lover"),
+                    new PlayRecord("Treasure Finder"),
+                    new PlayRecord("Treasure Seeker"),
+                    new PlayRecord("Treasure Hunter"),
+                }
+            },
+            {2, new List<PlayRecord>()
+                {
+                    new PlayRecord("Capsule Worker"),
+                    new PlayRecord("Capsule Warrior"),
+                    new PlayRecord("Capsule Wizard"),
+                    new PlayRecord("Baby Battler"),
+                    new PlayRecord("Big Battler"),
+                    new PlayRecord("Brilliant Battler"),
+                    new PlayRecord("Pro Battler"),
+                }
+            },
+            {3, new List<PlayRecord>()
+                {
+                    new PlayRecord("Famed Battler"),
+                    new PlayRecord("Legendary Battler"),
+                    new PlayRecord("Novice Team"),
+                    new PlayRecord("Able Team"),
+                    new PlayRecord("Skilled Team"),
+                    new PlayRecord("Veteran Team"),
+                    new PlayRecord("Formidable Team"),
+                    new PlayRecord("Dominating Team")
+                }
+            },
+            {4, new List<PlayRecord>()
+                {
+                    new PlayRecord("Spirit Friends"),
+                    new PlayRecord("Spirit Buddies"),
+                    new PlayRecord("Spirit Family"),
+                    new PlayRecord("Triple Chain"),
+                    new PlayRecord("Quadruple Chain"),
+                    new PlayRecord("Quintuple Chain"),
+                    new PlayRecord("Sextuple Chain"),
+                    new PlayRecord("Great Destruction")
+                }
+            },
+            {5, new List<PlayRecord>()
+                {
+                    new PlayRecord("Awesome Destruction"),
+                    new PlayRecord("Terrible Destruction"),
+                    new PlayRecord("Monstrous Destruction"),
+                    new PlayRecord("Immeasurable Destruction"),
+                    new PlayRecord("Unrivalled Destruction"),
+                    new PlayRecord("Earth Destroyer"),
+                    new PlayRecord("Galaxy Destroyer"),
+                    new PlayRecord("Striker")
+                }
+            },
+            {6, new List<PlayRecord>()
+                {
+                    new PlayRecord("Serious Striker"),
+                    new PlayRecord("Super Striker"),
+                    new PlayRecord("Ace Striker"),
+                    new PlayRecord("Miracle Striker"),
+                    new PlayRecord("Virtuoso"),
+                    new PlayRecord("Sample Data"),
+                    new PlayRecord("Helpful Data"),
+                    new PlayRecord("Reliable Data")
+                }
+            },
+            {7, new List<PlayRecord>()
+                {
+                    new PlayRecord("Treasure Master"),
+                    new PlayRecord("Treasure Legend"),
+                    new PlayRecord("Odaiba S-Ranker"),
+                    new PlayRecord("Extensive Data"),
+                    new PlayRecord("Flawless Data"),
+                    new PlayRecord("Systems Online"),
+                    new PlayRecord("In Demand"),
+                    new PlayRecord("Up to the Challenge")
+                }
+            },
+            {8, new List<PlayRecord>()
+                {
+                    new PlayRecord("Riverside S-Ranker"),
+                    new PlayRecord("Steel Tower S-Ranker"),
+                    new PlayRecord("Space Port S-Ranker"),
+                    new PlayRecord("Full Match S-Ranker"),
+                    new PlayRecord("Historical S-Ranker"),
+                    new PlayRecord("Revolutionary S-Ranker"),
+                    new PlayRecord("Space-Time S-Ranker"),
+                }
+            },
+            {9, new List<PlayRecord>()
+                {
+                    new PlayRecord("Limited S-Ranker"),
+                    new PlayRecord("Galaxy S-Ranker"),
+                    new PlayRecord("Linked S-Ranker"),
+                    new PlayRecord("Despair S-Ranker"),
+                    new PlayRecord("Friendship S-Ranker"),
+                    new PlayRecord("Ultimate S-Ranker"),
+                    new PlayRecord("Explosive Dribble Bronze Medal"),
+                    new PlayRecord("Explosive Dribble Silver Medal")
+                }
+            },
+            {10, new List<PlayRecord>()
+                {
+                    new PlayRecord("Explosive Dribble Gold Medal"),
+                    new PlayRecord("Hovering Rally Bronze Medal"),
+                    new PlayRecord("Hovering Rally Silver Medal"),
+                    new PlayRecord("Hovering Rally Gold Medal"),
+                    new PlayRecord("Bound Jump Bronze Medal"),
+                    new PlayRecord("Bound Jump Silver Medal"),
+                    new PlayRecord("Bound Jump Gold Medal"),
+                    new PlayRecord("Hovering Race Bronze Medal")
+                }
+            },
+            {11, new List<PlayRecord>()
+                {
+                    new PlayRecord("Totem Friends"),
+                    new PlayRecord("Totem Buddies"),
+                    new PlayRecord("TOtem Family"),
+                    new PlayRecord("Hovering Race Silver Medal"),
+                    new PlayRecord("Hovering Race Gold Medal")
+                }
+            },
+            {12, new List<PlayRecord>()
+                {
+                    new PlayRecord("Experienced Tactician"),
+                    new PlayRecord("Master Tactician"),
+                    new PlayRecord("Heavenly Tactician"),
+                    new PlayRecord("Baby Command Battler")
+                }
+            },
+            {13, new List<PlayRecord>()
+                {
+                    new PlayRecord("Famous Coaches"),
+                    new PlayRecord("Big Command Battler"),
+                    new PlayRecord("Brilliant Command Battler"),
+                    new PlayRecord("Pro Command Battler"),
+                    new PlayRecord("Famed Command Battler")
+                }
+            },
+            {14, new List<PlayRecord>()
+                {
+                    new PlayRecord("Trustworthy Coaches"),
+                    new PlayRecord("Legendary Coaches"),
+                    new PlayRecord("Shot Block Enthusiast"),
+                    new PlayRecord("Shot Block Specialist"),
+                    new PlayRecord("Shot Block Leader"),
+                    new PlayRecord("Counter-Attacker")
+                }
+            },
+            {15, new List<PlayRecord>()
+                {
+                    new PlayRecord("Director"),
+                    new PlayRecord("Commander"),
+                    new PlayRecord("Conductor"),
+                    new PlayRecord("Shot Chain Enthusiast"),
+                    new PlayRecord("Shot Chain Expert"),
+                    new PlayRecord("Shot Chain Guru"),
+                    new PlayRecord("Capable Scorer"),
+                    new PlayRecord("Agressive Scorer")
+                }
+            },
+            {16, new List<PlayRecord>()
+                {
+                    new PlayRecord("Amateur Item Collector"),
+                    new PlayRecord("Serious Item Collector"),
+                    new PlayRecord("Professional Item Collector"),
+                    new PlayRecord("Good Performer"),
+                    new PlayRecord("Super Performer"),
+                    new PlayRecord("Miracle Performer"),
+                    new PlayRecord("Dominant Scorer"),
+                    new PlayRecord("QR Code Explorer")
                 }
             },
         };
